54a7913bbe0bfcdbcba09331b7c752177364810d
Erhanero <erhancr7@gmail.com>
2022-02-06 09:00:26 +0200

    Add owner authentication for edit

--- controllers/edit.js
+++ controllers/edit.js
@@ -2,14 +2,28 @@ const carsServices = require("../services/carsServices");
 
 const get = async (req, res) => {
     const car = await carsServices.getById(req.params.id);
-    res.render("edit", { car });
 
+    if (car.owner != req.session.user.id) {
+        console.log("User is not owner!");
+        return res.redirect("/login");
+    }
+
+    res.render("edit", { car });
 }
 
 const post = async (req, res) => {
     const car = req.body;
-    await carsServices.editById(req.params.id, car);
-    res.redirect("/");
+    try {
+        if (await carsServices.editById(req.params.id, car, req.session.user.id)) {
+            res.redirect("/");
+        } else {
+            res.redirect("/login");
+        }
+
+    } catch (err) {
+        res.redirect("404")
+    }
+
 }
 
 module.exports = {
